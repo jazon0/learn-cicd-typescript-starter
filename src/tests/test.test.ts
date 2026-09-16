244f5389349b9ee23c4c8f5b4615801272d3ea7b
jazon0 <jason.peters@informatik.hs-fulda.de>
2026-09-16 20:18:23 +0200

    format checking ci

--- src/tests/test.test.ts
+++ src/tests/test.test.ts
@@ -1,23 +1,24 @@
 import { describe, expect, test } from "vitest";
 import { getAPIKey } from "../api/auth.js";
 
-
-
 describe("person", () => {
   test("getAPIKey to return result", () => {
-    expect(getAPIKey({
-      authorization: "ApiKey abc123"
-    })).toBe("abc123");
+    expect(
+      getAPIKey({
+        authorization: "ApiKey abc123",
+      }),
+    ).toBe("abc123");
   });
 
   test("getAPIKey wrong input", () => {
-    expect(getAPIKey({
-      "authorization": "wrong key"
-    })).toBe(null);
+    expect(
+      getAPIKey({
+        authorization: "wrong key",
+      }),
+    ).toBe(null);
   });
 
   test("getAPIKey no auth header", () => {
-    expect(getAPIKey({
-    })).toBe(null);
+    expect(getAPIKey({})).toBe(null);
   });
 });
